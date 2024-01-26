950f03184303f588b2062eecd6cc3bea177bc0f0
Ariel <ariel1995@gmail.com>
2024-01-25 16:10:52 -0800

    Setup linktoken dependency usage on tmol

--- README.md
+++ README.md
@@ -1,66 +1,15 @@
-## Foundry
-
-**Foundry is a blazing fast, portable and modular toolkit for Ethereum application development written in Rust.**
-
-Foundry consists of:
-
--   **Forge**: Ethereum testing framework (like Truffle, Hardhat and DappTools).
--   **Cast**: Swiss army knife for interacting with EVM smart contracts, sending transactions and getting chain data.
--   **Anvil**: Local Ethereum node, akin to Ganache, Hardhat Network.
--   **Chisel**: Fast, utilitarian, and verbose solidity REPL.
-
-## Documentation
-
-https://book.getfoundry.sh/
+## Requirements
+Install foundry
 
 ## Usage
+forge install smartcontractkit/chainlink-brownie-contracts
 
-### Build
+## Issues
+Guardar primary key de SEPOLIA 
+Guardar primary key de mi billetera
 
-```shell
-$ forge build
-```
+Interactions:
+ FundAPIConsumer with links (Add link token mock, install solmate to import ERC20)
 
-### Test
 
-```shell
-$ forge test
-```
 
-### Format
-
-```shell
-$ forge fmt
-```
-
-### Gas Snapshots
-
-```shell
-$ forge snapshot
-```
-
-### Anvil
-
-```shell
-$ anvil
-```
-
-### Deploy
-
-```shell
-$ forge script script/Counter.s.sol:CounterScript --rpc-url <your_rpc_url> --private-key <your_private_key>
-```
-
-### Cast
-
-```shell
-$ cast <subcommand>
-```
-
-### Help
-
-```shell
-$ forge --help
-$ anvil --help
-$ cast --help
-```
--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -4,9 +4,40 @@ pragma solidity ^0.8.19;
 
 import {APIConsumer} from "../src/APIConsumer.sol";
 import {Script, console} from "forge-std/Script.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+import {LinkToken} from "../test/mocks/LinkToken.sol";
 
-// contract FundAPIConsumer{
-//     function myFunc() public {
+contract FundAPIConsumer is Script {
+    function fundAPIConsumer(address apiConsumerAddress) public view {
+        //Send money to contract
+        if (block.chainid == 31337) {
+            console.log("Something");
+            console.log("Consumer API Address:", apiConsumerAddress);
+        } else {
+            return;
+            // vm.startBroadcast();
+            // LinkToken(link).transferAndCall(
+            //     /**address */,
+            //     /**Amount to fund */,
+            //     /*abi.encode(subId)*/
+            // );
+            // vm.stopBroadcast();
+        }
+    }
 
-//     }
-// }
+    function fundAPIConsumerUsingConfig() public {
+        HelperConfig helperConfig = new HelperConfig();
+        address apiConsumerAddress = helperConfig.activeNetworkConfig();
+        fundAPIConsumer(apiConsumerAddress);
+    }
+
+    function run() external {
+        fundAPIConsumerUsingConfig();
+    }
+}
+
+/**
+ * Interactions:
+ * FundAPIConsumer with links (Add link token mock, instalar solmate para poder importar ERC20)
+ * Aañdir mi llave privada
+ */
