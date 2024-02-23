dc3cbf69d35ecccac0376efbce413cd24cdd6e93
Ariel <ariel1995@gmail.com>
2024-02-23 10:59:14 -0800

    StartPoint before change to parameters list for createCUstomSLA function

--- test/ContractA.t.sol
+++ test/ContractA.t.sol
@@ -13,8 +13,25 @@ contract ContractTestTest is Test {
         example = deployer.run();
     }
 
-    function testContract() public {
+    function testContract() public pure {
         uint256[18] memory params;
         params[0] = 1;
+        params[1] = 2;
+        params[2] = 3;
+        params[3] = 4;
+        params[4] = 5;
+        params[5] = 6;
+        params[6] = 7;
+        params[7] = 8;
+        params[8] = 9;
+        params[9] = 10;
+        params[10] = 11;
+        params[11] = 12;
+        params[12] = 13;
+        params[13] = 14;
+        params[14] = 15;
+        params[15] = 16;
+        params[16] = 17;
+        params[17] = 18;
     }
 }
